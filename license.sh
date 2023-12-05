5f028e9a9e77d91efedd6649d0f8c71cd1d6e503
govardhan <saigov14@gmail.com>
2023-12-05 16:54:07 +0530

    add LICENSE to TB

--- license.sh
+++ license.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 
-LICENSE="verilog/LICENSE"
+# LICENSE="verilog/LICENSE"
+LICENSE="tb/LICENSE"
 
 # change this to *.sv for the systemverilog testbenches license update
-# finds the verilog files and reads them into verilog
-find . -name "*.v" | while read -r verilog ; do
+# finds the verilog/sysverilog files and reads them into verilog/sysverilog
+
+find . -name "*.sv" | while read -r sysverilog ; do
     # check the directory of the verilog file found in this iteration
-    dir=$(dirname $verilog) 
+    dir=$(dirname $sysverilog) 
     {  
-        echo "/*" ; echo "$(cat "$LICENSE")" ;  echo "*/" ; cat $verilog ;
-    } >  $dir/v_temp && mv $dir/v_temp $verilog
-    echo "Updated $verilog with License"
+        echo "/*" ; echo "$(cat "$LICENSE")" ;  echo "*/" ; cat $sysverilog ;
+    } >  $dir/sv_temp && mv $dir/sv_temp $sysverilog
+    echo "Updated $sysverilog with License"
 done
 echo "LICENSE added to all files"
